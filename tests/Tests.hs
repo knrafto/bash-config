1513156577241158055124e5808051f3ab56d19e
Kyle Raftogianis <kylerafto@gmail.com>
2013-12-08 23:41:20 -0800

    Allow multiple tests per file

--- tests/Tests.hs
+++ tests/Tests.hs
@@ -1,84 +1,145 @@
-{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveDataTypeable, LambdaCase #-}
 -- | Bash interpreter testing.
 --
--- Tests are specified as @tests/*.sh@ files. Each file consists of two Bash
--- scripts, separated by @\"###\"@ on a line by itself. Both scripts
--- are run and their resulting parameters are compared.
+-- Tests are specified as @tests/*.sh@ files. Each file consists of a
+-- series of pairs of scripts, separated by the line beginning with @###@.
+-- Each pair of scripts should result in the same environment.
 module Main ( main ) where
 
-import Control.Exception
-import Control.Monad
-import Data.Functor
-import Data.List
-import Data.Map (Map)
-import qualified Data.Map as Map
-import Data.Typeable
-import System.Directory
-import System.FilePath
-import System.Exit
-import System.IO
-import Test.Tasty
-import Test.Tasty.Providers
+import           Control.Applicative
+import           Control.Exception      (Exception)
+import qualified Control.Exception      as E
+import           Control.Monad
+import           Control.Monad.IO.Class
+import           Data.Char
+import           Data.List
+import           Data.Map               (Map)
+import qualified Data.Map               as Map
+import           Data.Typeable
+import           System.Directory
+import           System.FilePath
+import           System.Exit
+import           System.IO
+import           Test.Tasty
+import           Test.Tasty.Providers
+import           Text.Parsec.Combinator hiding (optional)
+import           Text.Parsec.Error
+import           Text.Parsec.Pos
+import           Text.Parsec.Prim       hiding ((<|>), parseTest)
 
-import Bash.Config
+import           Bash.Config            hiding (parse, interpret)
+import qualified Bash.Config            as Bash (parse, interpret)
 
 newtype TestFile = TestFile FilePath deriving (Typeable)
 
 instance IsTest TestFile where
-    run _ f _   = runTest f
+    run _ (TestFile file) _ = toResult <$> E.try (runTest file)
+      where
+        toResult (Left (TestFailed msg)) = Result False msg
+        toResult _                       = Result True ""
+
     testOptions = return []
 
-newtype TestFailed = TestFailed String deriving (Show, Typeable)
+newtype TestFailed = TestFailed String
+    deriving (Show, Typeable)
 
 instance Exception TestFailed
+
+data TestHeading = TestHeading String Line Line
+
+data TestPair = TestPair TestHeading Script Script
+
+failTest :: MonadIO m => String -> m a
+failTest = liftIO . E.throwIO . TestFailed
+
+modifyFailure :: (String -> String) -> IO a -> IO a
+modifyFailure f = E.handle $ \(TestFailed msg) -> failTest (f msg)
+
+indent :: Int -> String -> String
+indent n = unlines . map (replicate n ' ' ++) . lines
+
+parseTest :: FilePath -> IO [TestPair]
+parseTest file = do
+    ls <- lines <$> readFile file
+    runParserT (many1 pair <* eof) () file ls >>= \case
+        Left  _  -> failTest "unmatched test parts"
+        Right ps -> return ps
+  where
+    satisfy p = tokenPrim id updatePos test
+      where
+        updatePos pos _ _  = incSourceLine pos 1
+        test c | p c       = Just c
+               | otherwise = Nothing
+
+    testLine  = satisfy (not . isTestSep)
+    testSep   = dropWhile isSpace . drop 3 <$> satisfy isTestSep
+    isTestSep = ("###" `isPrefixOf`)
+
+    lineNumber = sourceLine <$> getPosition
+
+    pair = do
+        l1 <- lineNumber
+        (name, s1) <- script
+        (_   , s2) <- script
+        l2 <- subtract 1 <$> lineNumber
+        return $ TestPair (TestHeading name l1 l2) s1 s2
+
+    script = do
+        name <- testSep
+        offset <- subtract 1 <$> lineNumber
+        ls <- many1 testLine
+        case Bash.parse file (unlines ls) of
+            Left  e -> failTest . show $ incParseErrorLine offset e
+            Right s -> return (name, s)
+
+    incParseErrorLine offset e =
+        setErrorPos (incSourceLine (errorPos e) offset) e
 
 testFile :: FilePath -> TestTree
 testFile file = singleTest (takeBaseName file) (TestFile file)
 
-runTest :: TestFile -> IO Result
-runTest (TestFile file) = catchResult $ do
-    (lines1, lines2) <- break (== "###") . lines <$> readFile file
-    let script1 = unlines lines1
-        script2 = unlines $ replicate (length lines1) "" ++ lines2
-    env1 <- runScript file script1
-    env2 <- runScript file script2
-    let message = diff (parameters env1) (parameters env2)
-    unless (null message) $ throwIO (TestFailed message)
+runTest :: FilePath -> IO ()
+runTest file = do
+    pairs <- parseTest file
+    mapM_ testPair pairs
+
+testPair :: TestPair -> IO ()
+testPair (TestPair heading s1 s2) = withHeading heading $ do
+    [e1, e2] <- mapM execute [s1, s2]
+    let msg = diff (parameters e1) (parameters e2)
+    unless (null msg) $ failTest msg
   where
-    catchResult m = toResult <$> try m
+    withHeading (TestHeading name l1 l2) = modifyFailure $ \s ->
+        name ++ (if null name then "" else " ") ++
+        "(" ++ show l1 ++ "-" ++ show l2 ++ "):\n" ++
+        indent 2 s
 
-    toResult (Left (TestFailed msg)) = Result False msg
-    toResult _                       = Result True ""
-
-runScript :: FilePath -> String -> IO Env
-runScript name s = do
-    script <- case parse name s of
-        Left  e -> throwIO (TestFailed $ show e)
-        Right r -> return r
-    case interpret script emptyEnv of
-        Nothing  -> throwIO (TestFailed "execution failed")
-        Just env -> return env
+    execute s = case Bash.interpret s emptyEnv of
+        Nothing -> failTest "execution failed"
+        Just e  -> return e
 
 diff :: Map String Value -> Map String Value -> String
 diff a b = concatMap report $ Map.keys (a `Map.union` b)
   where
     report k
         | v1 == v2  = ""
-        | otherwise = "expected: " ++ showValue v2 ++ "\n" ++
-                      " but got: " ++ showValue v1 ++ "\n"
+        | otherwise = "expected: " ++ showElem v2 ++ "\n" ++
+                      " but got: " ++ showElem v1 ++ "\n"
       where
         v1 = Map.lookup k a
         v2 = Map.lookup k b
 
-        showValue Nothing           = "unknown " ++ k
-        showValue (Just (Value v )) = k ++ "=" ++ show v
-        showValue (Just (Array vs)) = k ++ "=(" ++ intercalate " " vs ++ ")"
+        showElem Nothing  = "unknown " ++ k
+        showElem (Just v) = k ++ "=" ++ showValue v
+
+        showValue (Value v)  = show v
+        showValue (Array vs) = "(" ++ intercalate " " (map show vs) ++ ")"
 
 prepareTests :: IO TestTree
 prepareTests = do
-    setCurrentDirectory "tests"
+    dir <- getCurrentDirectory
     cases <- filter (\f -> takeExtension f == ".sh") <$>
-             getDirectoryContents "."
+             getDirectoryContents dir
     when (null cases) $ do
         hPutStrLn stderr "error: no test cases found"
         exitFailure
@@ -86,5 +147,6 @@ prepareTests = do
 
 main :: IO ()
 main = do
+    setCurrentDirectory "tests"
     tests <- prepareTests
     defaultMain tests
